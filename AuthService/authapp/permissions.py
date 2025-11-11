3d57240f4871f6c6d1af1e37093fa73bbb0b7dd1
triptijain07 <triptijain0731@gmail.com>
2025-11-11 17:45:05 +0530

    Delete AuthService directory

--- AuthService/authapp/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsCustomer(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'customer'
-
-class IsShopOwner(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'shop_owner'
